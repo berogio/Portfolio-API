8a8abb7ef979ea837a273b724e3c6fecc2c53cdf
berogio <giorgi.beridze01e@geolab.edu.ge>
2024-01-22 22:36:12 +0100

    AddedNodemailer

--- src/contact/contact.controller.ts
+++ src/contact/contact.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ContactService } from './contact.service';
 
 @Controller('contact')
-export class ContactController {}
+export class ContactController {
+  constructor(private readonly contactService: ContactService) {}
+
+  @Post()
+  create(@Body() requestBody: any) {
+    this.contactService.sendMail(requestBody.email, requestBody.message);
+    return { message: 'Data received and processed successfully' };
+  }
+}
--- src/contact/contact.module.ts
+++ src/contact/contact.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ContactController } from './contact.controller';
 import { ContactService } from './contact.service';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        service: 'gmail',
+        auth: {
+          user: 'giorgi.beridze01e@geolab.edu.ge',
+          pass: 'Beridzeo1j',
+        },
+      },
+    }),
+  ],
+
   controllers: [ContactController],
   providers: [ContactService],
 })
