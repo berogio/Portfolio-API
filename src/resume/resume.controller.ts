fbe5492e5d7addaa08a8eeaf93b2387f36ecdbce
berogio <giorgi.beridze01e@geolab.edu.ge>
2024-02-01 00:48:46 +0100

    testttt

--- src/resume/resume.controller.ts
+++ src/resume/resume.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, Res } from '@nestjs/common';
+import { Response } from 'express';
 
 @Controller('resume')
-export class ResumeController {}
+export class ResumeController {
+  @Get()
+  async getPicture(@Query('filename') filename: string, @Res() res: Response) {
+    if (!filename) {
+      return res.status(400).send('Missing filename');
+    }
+
+    try {
+      res.sendFile(filename, { root: './public' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  }
+}
