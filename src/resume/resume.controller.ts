d5fe818c02232118f664a2cfb9aa5eea226b1c2a
berogio <giorgi.beridze01e@geolab.edu.ge>
2024-02-01 16:52:33 +0100

    allesget

--- src/resume/resume.controller.ts
+++ src/resume/resume.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { join } from 'path';
 
@@ -10,11 +10,21 @@ export class ResumeController {
       const currentDir = process.cwd();
       const filePath = join(currentDir, 'public', 'example.pdf');
       res.setHeader('Content-Type', 'application/pdf');
-      res.setHeader('Content-Disposition', 'inline; filename=example.pdf');
       res.sendFile(filePath);
     } catch (error) {
       console.error(error);
       res.status(500).send('Internal Server Error');
+    }
+  }
+  @Post()
+  async postPassword(@Req() req: Request, @Res() res: Response) {
+    try {
+      const inputData = req.body;
+      console.log('Received POST request with data:', inputData);
+      res.status(200).json({ message: 'POST request received successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal Server Error' });
     }
   }
 }
