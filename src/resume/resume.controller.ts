d9856b8cb3c4c6b6a4ccae27e74c2accff9a9587
berogio <giorgi.beridze01e@geolab.edu.ge>
2024-02-01 01:20:15 +0100

    test

--- src/resume/resume.controller.ts
+++ src/resume/resume.controller.ts
@@ -1,16 +1,21 @@
-import { Controller, Get, Query, Res } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
+import { join } from 'path';
+import { existsSync } from 'fs';
 
 @Controller('resume')
 export class ResumeController {
   @Get()
-  async getPicture(@Query('filename') filename: string, @Res() res: Response) {
-    if (!filename) {
-      return res.status(400).send('Missing filename');
-    }
-
+  async getResume(@Res() res: Response) {
+    const publicFolderPath = join(process.cwd(), 'public');
+    const filename = 'example.pdf';
+    const filePath = join(publicFolderPath, filename);
     try {
-      res.sendFile(filename, { root: './public' });
+      if (!existsSync(filePath)) {
+        return res.status(404).send('File not found');
+      }
+      res.setHeader('Content-Type', 'application/pdf');
+      res.sendFile(filePath);
     } catch (error) {
       console.error(error);
       res.status(500).send('Internal Server Error');
