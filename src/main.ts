5f6264c4504b31f7194edbd99983dbb147dc996a
berogio <giorgi.beridze01e@geolab.edu.ge>
2024-02-06 01:09:35 +0100

    t

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(compression());
   app.enableCors();
-  await app.listen(process.env.PORT, '0.0.0.0');
-  // await app.listen(3000);
+  // await app.listen(process.env.PORT, '0.0.0.0');
+  await app.listen(3000);
 }
 bootstrap();
