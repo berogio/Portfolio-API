13794ccc9af761649f5c28b9a1b8f0df1da75ae0
berogio <giorgi.beridze01e@geolab.edu.ge>
2024-02-06 01:10:51 +0100

    FUCK

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(compression());
   app.enableCors();
-  // await app.listen(process.env.PORT, '0.0.0.0');
-  await app.listen(3000);
+  await app.listen(process.env.PORT, '0.0.0.0');
+  // await app.listen(3000);
 }
 bootstrap();
